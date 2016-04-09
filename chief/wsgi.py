9d48502180c79246de59230c67b1fbd1e0af3de0
Ben <rudolphben@gmail.com>
2016-04-09 16:51:26 -0400

    whiteness again

--- chief/wsgi.py
+++ chief/wsgi.py
@@ -2,8 +2,10 @@ import os
 from manage import _set_source_root_parent
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chief.settings")
 _set_source_root_parent('submodules')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
