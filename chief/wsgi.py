491e489fa339b390fcd9835c4358dc451fdfaa26
Ben <rudolphben@gmail.com>
2016-04-09 14:06:16 -0400

    add deploy conf

--- chief/wsgi.py
+++ chief/wsgi.py
@@ -1,7 +1,9 @@
 import os
+from manage import _set_source_root_parent
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chief.settings")
+_set_source_root_parent('submodules')
 
 application = get_wsgi_application()
