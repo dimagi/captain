66955141b18a5ebb30dbad2017dd2aaa538122d0
Ben <rudolphben@gmail.com>
2016-04-30 23:27:41 -0700

    Actually add static files

---
+++ apps/deploy/static/captain/js/captain.js
@@ -0,0 +1,38 @@
+(function() {
+    "strict"
+
+    window.Captain = {
+        ViewModels: {}
+    };
+
+    Captain.ViewModels.InitiateDeploy = function() {
+        var self = this;
+        self.env = ko.observable(null);
+
+        $('#deploy-modal').on('show.bs.modal', function(e) {
+            var $btn = $(e.relatedTarget);
+            self.env($btn.data('environment'));
+        });
+
+        self.defaultBranch = function() {
+            if (self.env() === 'staging') {
+                return 'autostaging';
+            }
+            return 'master';
+        };
+
+        self.initiateDeploy = function() {
+            $('#deploy-form').submit();
+        };
+    };
+
+    $('#stacktrace-modal').on('show.bs.modal', function(e) {
+        var $btn = $(e.relatedTarget),
+            stacktrace = $btn.data('stacktrace');
+
+        $('#stacktrace-modal').find('.stacktrace').html(
+            stacktrace.replace(/(?:\r\n|\r|\n)/g, '<br />')
+        );
+    });
+
+})();
